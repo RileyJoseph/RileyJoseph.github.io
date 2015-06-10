b118564176487bcfe3bce6dc3d7bf67f9a7b4150
RileyJoseph <riley.m.joseph1@gmail.com>
2015-06-10 01:07:50 -0700

    added scroll reveal

--- js/jq.js
+++ js/jq.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $('#myName').slideDown(1000);
-    $('#nB3').slideDown(1200);
-    $('#nB4').slideDown(1600);
-    $('#nB5').slideDown(2000);
-    $('#rj').fadeIn(4000);
+    // $('#myName').slideDown(1000);
+    // $('#nB3').slideDown(1200);
+    // $('#nB4').slideDown(1600);
+    // $('#nB5').slideDown(2000);
+    // $('#rj').fadeIn(4000);
     // $("#rj").bind("load", function () { $(this).fadeIn(); });
 });
